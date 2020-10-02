c515f20221ba265d3a0ee4667850945d41cda8eb
jekostud <tillerman9@gmail.com>
2020-10-02 17:07:21 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+// smallest
+exports.min = function min(array) {
+  let minValue = array
+      ? array.reduce((smallest, current) => Math.min(smallest, current), 0)
+      : 0;
 
-exports.max = function max (array) {
-  return 0;
-}
+  return minValue;
+};
 
-exports.avg = function avg (array) {
+// biggest
+exports.max = function max(array) {
+  let maxValue = array
+      ? array.reduce((biggest, current) => Math.max(biggest, current), 0)
+      : 0;
+
+  return maxValue;
+};
+
+// average
+exports.avg = function avg(array) {
+  if (array && array.length !== 0) {
+      let average = 0;
+      average = array.reduce((sum, current) => (sum += current), 0);
+      return average / array.length;
+  }
   return 0;
 }
